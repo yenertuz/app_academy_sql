fcf7585f2d9fca4161b75137e0dbf81ee393095f
yenertuz <yenertuz@hotmail.com>
2019-01-04 14:20:36 -0800

    part 3.1 of 03_aa_questions

--- 03_aa_questions/questions.rb
+++ 03_aa_questions/questions.rb
@@ -36,6 +36,29 @@ class User
 		return nil if data.length == 0
 		User.new(data[0])
 	end
+	
+	def self.find_by_name(fname, lname)
+		data = PlayDBConnection.instance.execute(<<-SQL, fname, lname)
+		SELECT * FROM users WHERE fname = ? AND lname = ?;
+		SQL
+		data.map { |datum|  User.new(datum)       }
+	end
+
+	def authored_questions
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @id)
+		SELECT * FROM questions WHERE user_id = ? ;
+		SQL
+		data.map { |datum| Question.new(datum)  }
+	end
+
+	def authored_replies
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @id)
+		SELECT * FROM replies WHERE user_id = ? ;
+		SQL
+		data.map {  |datum| Reply.new(datum)       }
+	end
 
 end
 
@@ -55,6 +78,30 @@ class Question
 		SQL
 		return nil if data.length == 0
 		Question.new(data[0])
+	end
+
+	def self.find_by_author_id(id)
+		data = PlayDBConnection.instance.execute(<<-SQL, id)
+		SELECT * FROM questions WHERE user_id = ? ;
+		SQL
+		data.map { |datum| Question.new(datum)   }
+	end
+
+	def author
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @user_id)
+		SELECT * FROM users WHERE id = ? ;
+		SQL
+		return nil if data.length == 0
+		User.new(data[0])
+	end
+
+	def replies
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @id)
+		SELECT * FROM replies WHERE question_id = ? ;
+		SQL
+		data.map { |datum| Reply.new(datum)   }
 	end
 
 end
@@ -95,6 +142,55 @@ class Reply
 		SQL
 		return nil if data.length == 0
 		Reply.new(data[0])
+	end
+
+	def self.find_by_user_id(id)
+		data = PlayDBConnection.instance.execute(<<-SQL, id)
+		SELECT * FROM replies WHERE user_id = ? ;
+		SQL
+		data.map { |datum| Reply.new(datum)   }
+	end
+
+	def self.find_by_question_id(id)
+		data = PlayDBConnection.instance.execute(<<-SQL, id)
+		SELECT * FROM replies WHERE question_id = ? ;
+		SQL
+		data.map { |datum| Reply.new(datum)   }
+	end
+
+	def author
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @user_id)
+		SELECT * FROM users WHERE id = ? ;
+		SQL
+		return nil if data.length == 0
+		User.new(data[0])
+	end
+
+	def question 
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @question_id)
+		SELECT * FROM questions WHERE id = ? ;
+		SQL
+		return nil if data.length == 0
+		Question.new(data[0])
+	end
+
+	def parent_reply
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @reply_id)
+		SELECT * FROM replies WHERE id = ? ;
+		SQL
+		return nil if data.length == 0
+		Reply.new(data[0])
+	end
+
+	def child_replies
+		raise "#{self} not in database" if @id == nil
+		data = PlayDBConnection.instance.execute(<<-SQL, @id)
+		SELECT * FROM replies WHERE reply_id = ? ;
+		SQL
+		data.map { |datum| Reply.new(datum) }
 	end
 
 end
