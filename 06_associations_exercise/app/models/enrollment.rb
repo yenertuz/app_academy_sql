8b23e137f7a1c229af2bf1aaab7498df2d35295d
yenertuz <yenertuz@hotmail.com>
2019-01-06 16:28:54 -0800

    finish associations_exercise

--- 06_associations_exercise/app/models/enrollment.rb
+++ 06_associations_exercise/app/models/enrollment.rb
@@ -1,2 +1,14 @@
 class Enrollment < ApplicationRecord
+	
+	belongs_to :user,
+		primary_key: :id,
+		foreign_key: :student_id,
+		class_name: :User
+
+	belongs_to :course,
+		primary_key: :id,
+		foreign_key: :course_id,
+		class_name: :Course
+	
+	
 end
