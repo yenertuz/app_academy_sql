38135960fb2685ad81a347dd5793e73e93ef3f11
yenertuz <yenertuz@hotmail.com>
2019-01-09 18:52:22 -0800

    finish 08_movie_buff_in_training

--- 08_movie_buff_in_training/config/application.rb
+++ 08_movie_buff_in_training/config/application.rb
@@ -6,10 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# config.autoload_paths += %W( #{config.root}/movie_novice/queries.rb )
+
+#load (Rails.root.to_s + "/movie_novice/queries.rb")
+
 module MovieBuff
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+	config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- 08_movie_buff_in_training/movie_novice/queries.rb
+++ 08_movie_buff_in_training/movie_novice/queries.rb
@@ -23,19 +23,19 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
-
+  Actor.find_by(name: "Angelina Jolie")
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  Movie.select(:id, :title).where("score >= ?", 9)
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-
+  Movie.select(:id, :title, :yr).where("title like '%Star Wars%'")
 end
 
 
@@ -44,7 +44,7 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
-
+Movie.select("yr, count(id) as bad_movies").where("score < 5").group("yr").order("bad_movies DESC")
 end
 
 def alphabetized_actors
@@ -53,14 +53,14 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+Actor.order(:name).limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+Movie.find_by(title: "Pulp Fiction").actors
 end
 
 def uma_movies
@@ -68,5 +68,5 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Actor.find_by("name like '%Uma%'").movies.select(:id, :title, :yr).order(yr: :asc)
 end
